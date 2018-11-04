3e0579ed1c32f2fa8bfa4f81c7e654ee51c927f0
Mark Packham <markpackham1@gmail.com>
2018-11-04 22:03:19 +0000

    Drupal Symfony service & injection learnt from https://www.youtube.com/watch?v=M7fZT0tfxV4

---
+++ modules/custom/drupalup_service/src/CowService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\drupalup_service;
+
+/*
+ * CowService is a simple example of a Drupal 8 service
+ */
+
+use Drupal\Core\Session\AccountProxy;
+
+class CowService {
+
+	private $sounds = [ "moo", "meh" ];
+	private $currentUser;
+
+	/*
+	 * use the current_user service from drupal.org
+	 * we want to do an injection into our service with the current user
+	 */
+	public function __construct( AccountProxy $currentUser ) {
+		$this->currentUser = $currentUser;
+	}
+
+	/*
+	 * Returns a cow sound
+	 */
+	public function saySomething() {
+		return $this->sounds[ array_rand( $this->sounds ) ];
+	}
+
+	/*
+	 * Returns the cow owner
+	 */
+	public function whoIsYourOwner() {
+		return ( $this->currentUser->getDisplayName() );
+	}
+}
