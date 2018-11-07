03b80f9d85b8a8366f63d9d4eb8ab63d60a807f1
Mark Packham <markpackham1@gmail.com>
2018-11-07 16:51:12 +0000

    add more info

--- modules/custom/drupalup_jsset/js/jsset.js
+++ modules/custom/drupalup_jsset/js/jsset.js
@@ -8,7 +8,11 @@ Contains JS functions
     Drupal.behaviors.jsDrupalupTest = {
         attach: function (context, settings) {
             //append to the js-var element
-            $('.js-var').once('jsDrupalupTest').append('<button class="button">' + drupalSettings.js_example.title + '</button>');
+            $('.js-var').once('jsDrupalupTest').append('' +
+                '<p>The button changes based on the site name: /admin/config/system/site-information</p>' +
+                '<button class="button">' + drupalSettings.js_example.title + '</button>'
+                + '<p>We have a console.log "ping" message to prove the .once from jquery.once works</p>'
+            );
             console.log('ping');
         }
     }
