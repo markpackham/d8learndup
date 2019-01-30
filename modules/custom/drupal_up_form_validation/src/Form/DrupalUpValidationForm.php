9dc2ea60efdd4279a84d61d25c4330c91afb1075
Mark Packham <markpackham1@gmail.com>
2019-01-30 11:49:44 +0000

    format the code & finish it off

--- modules/custom/drupal_up_form_validation/src/Form/DrupalUpValidationForm.php
+++ modules/custom/drupal_up_form_validation/src/Form/DrupalUpValidationForm.php
@@ -7,39 +7,56 @@ use Drupal\Core\Form\FormStateInterface;
 
 /*
 Drupal form with an example of form validation
-*/
-class DrupalUpValidationForm extends FormBase{
+ */
+class DrupalUpValidationForm extends FormBase
+{
 
 /**
-*{@inheritdoc}
-*/
+ *{@inheritdoc}
+ */
     public function getFormId()
     {
         return 'drupal_up_validation_form';
     }
 
+    /**
+     *{@inheritdoc}
+     */
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        $form['name'] = [
+            '#type' => 'textfield',
+            '#title' => $this->t('Name'),
+            '#required' => true,
+        ];
+        $form['submit'] = [
+            '#type' => 'submit',
+            '#value' => $this->t('Please say my name'),
+        ];
+        return $form;
+    }
+
+/*
+Custom validation
+ */
+    public function validateForm(array &$form, FormStateInterface $form_state)
+    {
+        if (strlen($form_state->getValue('name')) < 4) {
+            $form_state->setErrorByName(
+                'name',
+                $this->t('Your name should be longer than 3 characters or I will not say it')
+            );
+        }
+    }
 
     /**
-*{@inheritdoc}
-*/
-public function buildForm(array $form, FormStateInterface $form_state)
-{
-    $form['name']=[
-        '#type'=>'textfield',
-        '#title'=>$this->t('Name'),
-    ];
-    $form['submit']=[
-    '#type'=>'submit',
-    '#value'=>$this->t('Please say my name'),
-    ];
-return $form;
-}
-
-public function submitForm(array &$form, FormStateInterface $form_state)
-{
-    drupal_set_message(
-$this->t('Your name is ').$form_state->getValue('name')
-    );
-}
+     *{@inheritdoc}
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        drupal_set_message(
+            $this->t('Your name is ') . $form_state->getValue('name')
+        );
+    }
 
 }
