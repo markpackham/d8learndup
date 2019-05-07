f0b1097175a544c15af619bf1a3ba4a3d23b2f84
Mark Packham <markpackham1@gmail.com>
2019-05-07 12:39:42 +0100

    use the Account Proxy Interface for greater code flexability https://www.youtube.com/watch?v=50joYie4oFk

--- modules/custom/drupalup_service/src/CowService.php
+++ modules/custom/drupalup_service/src/CowService.php
@@ -6,32 +6,36 @@ namespace Drupal\drupalup_service;
  * CowService is a simple example of a Drupal 8 service
  */
 
-use Drupal\Core\Session\AccountProxy;
+use Drupal\Core\Session\AccountProxyInterface;
 
-class CowService {
+class CowService
+{
 
-	private $sounds = [ "moo", "meh" ];
-	private $currentUser;
+    private $sounds = ["moo", "meh"];
+    private $currentUser;
 
-	/*
-	 * use the current_user service from drupal.org
-	 * we want to do an injection into our service with the current user
-	 */
-	public function __construct( AccountProxy $currentUser ) {
-		$this->currentUser = $currentUser;
-	}
+    /*
+     * use the current_user service from drupal.org
+     * we want to do an injection into our service with the current user
+     */
+    public function __construct(AccountProxyInterface $currentUser)
+    {
+        $this->currentUser = $currentUser;
+    }
 
-	/*
-	 * Returns a cow sound
-	 */
-	public function saySomething() {
-		return $this->sounds[ array_rand( $this->sounds ) ];
-	}
+    /*
+     * Returns a cow sound
+     */
+    public function saySomething()
+    {
+        return $this->sounds[array_rand($this->sounds)];
+    }
 
-	/*
-	 * Returns the cow owner
-	 */
-	public function whoIsYourOwner() {
-		return ( $this->currentUser->getDisplayName() );
-	}
+    /*
+     * Returns the cow owner
+     */
+    public function whoIsYourOwner()
+    {
+        return ($this->currentUser->getDisplayName());
+    }
 }
